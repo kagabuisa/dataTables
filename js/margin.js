992b1ae3ed0afac922d9115466fd4de484b81fe8
kagabuisa <kagabuisa@gmail.com>
2018-12-01 18:37:25 +0300

    commit margin report

---
+++ js/margin.js
@@ -0,0 +1,32 @@
+$('document').ready(function () {
+    //"example" in ('#example') is the table id, this is declared under table class
+    $('#example').DataTable({
+        "ajax": "JSON/Margins-report.json",
+        "dataSrc": "",
+        "columns": [{
+                "data": "Date"
+            },
+            {
+                "data": "CSTG"
+            },
+            {
+                "data": "Sales"
+            },
+            {
+                "data": "COGs"
+            },
+            {
+                "data": "Profit"
+            },
+            {
+                "data": "Margin"
+            }
+
+
+        ],
+        //"dom": "Bftrip",
+        //"buttons": ['excel', 'pdf', 'print']
+
+    });
+
+});
